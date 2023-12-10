b8a391bf58a1d3497a20810166d4e170f70c67fe
Sam Lijin <github@sxlijin.com>
2023-12-09 18:11:21 -0700

    feat: move auto-download into build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,44 @@
 
 set -euxo pipefail
 
-export PANDOC_CLI
-export SASS_CLI
+export BIN_DIR=_bin
 export OUTPUT_DIR=_site
+
+mkdir -p ${BIN_DIR}
 
 # Avoid removing the output directory itself: this screws with hot reload.
 rm -rf ${OUTPUT_DIR}/*
 mkdir -p ${OUTPUT_DIR}
+
+##############################################################################
+#                                                                            #
+#  Auto-download pandoc and dart-sass CLIs                                   #
+#                                                                            #
+##############################################################################
+
+if [ ! -e "${BIN_DIR}/pandoc.tar.gz" ]
+then
+  curl \
+    -L https://github.com/jgm/pandoc/releases/download/3.1.9/pandoc-3.1.9-linux-amd64.tar.gz \
+    -o "${BIN_DIR}/pandoc.tar.gz"
+  tar xf "${BIN_DIR}/pandoc.tar.gz" -C "${BIN_DIR}"
+fi
+export PANDOC_CLI="$(pwd)/${BIN_DIR}/pandoc-3.1.9/bin/pandoc"
+
+if [ ! -e "${BIN_DIR}/dart-sass.tar.gz" ]
+then
+  curl \
+    -L https://github.com/sass/dart-sass/releases/download/1.69.5/dart-sass-1.69.5-linux-x64.tar.gz \
+    -o "${BIN_DIR}/dart-sass.tar.gz"
+  tar xf "${BIN_DIR}/dart-sass.tar.gz" -C "${BIN_DIR}"
+fi
+export SASS_CLI="$(pwd)/${BIN_DIR}/dart-sass/sass"
+
+##############################################################################
+#                                                                            #
+#  Build all assets                                                          #
+#                                                                            #
+##############################################################################
 
 # Everything in root assets used to be at the root of the repo. Keeping it that
 # way for backwards compatibility, just in case there's a reason I wanted them
