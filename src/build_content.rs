aa2247c86586c24d1255c8953d26de0165afe7fc
Sam Lijin <dev@sxlijin.com>
2025-04-24 02:42:06 -0700

    mostly there, trying to clean up the rendering code now though

--- src/build_content.rs
+++ src/build_content.rs
@@ -2,7 +2,9 @@ use anyhow::{Context, Result};
 use askama::Template;
 use chrono::NaiveDate;
 use comrak::Arena;
+use serde::de::DeserializeOwned;
 use serde::Deserialize;
+use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
 use walkdir::WalkDir;
@@ -97,6 +99,295 @@ pub fn build_scss() -> Result<()> {
     Ok(())
 }
 
+struct MarkdownRenderEngine<'a> {
+    options: comrak::Options<'a>,
+    arena: comrak::Arena<comrak::nodes::AstNode<'a>>,
+}
+
+struct ParsedMarkdown<'md, TFrontmatter> {
+    frontmatter: TFrontmatter,
+    title: String,
+    body: &'md comrak::nodes::AstNode<'md>,
+}
+
+impl<'a> MarkdownRenderEngine<'a> {
+    fn new() -> Self {
+        Self {
+            options: comrak::Options {
+                extension: comrak::ExtensionOptions::builder()
+                    .front_matter_delimiter("---".into())
+                    .autolink(true)
+                    .build(),
+                parse: comrak::ParseOptions::default(),
+                render: comrak::RenderOptions::builder().unsafe_(true).build(),
+            },
+            arena: comrak::Arena::new(),
+        }
+    }
+
+    fn parse_markdown<TFrontmatter: DeserializeOwned>(
+        &'a self,
+        markdown: String,
+    ) -> Result<ParsedMarkdown<'a, TFrontmatter>> {
+        let root = comrak::parse_document(&self.arena, &markdown, &self.options);
+
+        let mut frontmatter = None;
+        let mut h1: Option<String> = None;
+        for child in root.children() {
+            if let comrak::nodes::NodeValue::FrontMatter(ref fm) = child.data.borrow().value {
+                let fm = fm.split("---\n").collect::<Vec<&str>>().join("");
+                let fm: TFrontmatter = serde_yaml::from_str(&fm)?;
+                frontmatter = Some(fm);
+            }
+            if let comrak::nodes::NodeValue::Heading(_) = child.data.borrow().value {
+                let h1_ref = &child.first_child().unwrap().data.borrow();
+                h1 = Some(h1_ref.value.text().clone().unwrap().clone());
+                break;
+            }
+        }
+
+        Ok(ParsedMarkdown {
+            frontmatter: frontmatter.context("Failed to parse frontmatter")?,
+            title: h1.context("Failed to parse title")?,
+            body: root,
+        })
+    }
+
+    fn build_posts(&'a self) -> Result<Vec<PostMetadata>> {
+        let mut posts = Vec::new();
+
+        // Ensure the output directory exists
+        let output_dir = Path::new("_site");
+        if !output_dir.exists() {
+            fs::create_dir_all(output_dir)?;
+        }
+
+        // Check if the posts directory exists
+        let posts_dir = Path::new("posts");
+        if !posts_dir.exists() {
+            anyhow::bail!("Posts directory not found");
+        }
+
+        // Use walkdir to iterate through all files in the posts directory
+        for entry in WalkDir::new(posts_dir)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file())
+        {
+            let path = entry.path();
+
+            // Only process .md files
+            if path.extension().and_then(|s| s.to_str()) == Some("md") {
+                // Get the relative path from the posts directory
+                let relative_path = path.strip_prefix(posts_dir)?;
+                let output_path = output_dir.join(relative_path.with_extension("html"));
+                let output_path2 = output_dir
+                    .join(relative_path.with_extension(""))
+                    .join("index.html");
+
+                // Ensure the parent directory exists
+                if let Some(parent) = output_path.parent() {
+                    fs::create_dir_all(parent)?;
+                }
+
+                tracing::info!("Converting {} to {}", path.display(), output_path.display());
+
+                // Read the markdown file
+                let markdown = fs::read_to_string(path)?;
+
+                let root = comrak::parse_document(&self.arena, &markdown, &self.options);
+                for child in root.children() {
+                    if let comrak::nodes::NodeValue::Heading(_) = child.data.borrow().value {
+                        let h1_ref = &child.first_child().unwrap().data.borrow();
+                        let h1 = h1_ref.value.text().clone().unwrap();
+
+                        posts.push(PostMetadata {
+                            timestamp: NaiveDate::parse_from_str(
+                                &path.file_name().unwrap().to_str().unwrap()[..10],
+                                "%Y-%m-%d",
+                            )?,
+                            title: h1.clone(),
+                            path: path
+                                .file_stem()
+                                .expect("Failed to get file stem")
+                                .to_str()
+                                .unwrap()
+                                .to_string(),
+                        });
+                        break;
+                    }
+                }
+                let mut buf = Vec::new();
+                comrak::format_html(&root, &self.options, &mut buf)?;
+                let html = String::from_utf8(buf)
+                    .context("Failed to convert markdown to HTML (UTF8 validation failed)")?;
+                let mut html = html.lines().collect::<Vec<&str>>();
+                let formatted_date = &format!(
+                    "<p>{}</p>",
+                    posts.last().unwrap().timestamp.format("%Y %b %d")
+                );
+                html.insert(1, formatted_date);
+                html.insert(2, "<hr/>");
+                let html = Page {
+                    title: posts.last().unwrap().title.clone(),
+                    css: "".to_string(),
+                    content: html.join("\n"),
+                }
+                .render()
+                .context("Failed to render template")?;
+
+                // Write the HTML to the output file
+                if let Some(parent) = output_path.parent() {
+                    fs::create_dir_all(parent)?;
+                }
+                if let Some(parent) = output_path2.parent() {
+                    fs::create_dir_all(parent)?;
+                }
+                fs::write(output_path, html.clone())?;
+                fs::write(output_path2, html)?;
+            }
+        }
+
+        tracing::debug!("Markdown conversion completed successfully");
+        Ok(posts)
+    }
+
+    fn build_index_html(&'a self, posts: Vec<PostMetadata>) -> Result<()> {
+        // Ensure the output directory exists
+        let output_dir = Path::new("_site");
+        if !output_dir.exists() {
+            fs::create_dir_all(output_dir)?;
+        }
+
+        // Check if the posts directory exists
+        let path = Path::new("pages/index.md");
+        if !path.exists() {
+            anyhow::bail!("Pages directory not found");
+        }
+
+        let output_path = output_dir.join("index.html");
+
+        // Ensure the parent directory exists
+        if let Some(parent) = output_path.parent() {
+            fs::create_dir_all(parent)?;
+        }
+
+        tracing::info!("Converting {} to {}", path.display(), output_path.display());
+
+        // Read the markdown file
+        let markdown = fs::read_to_string(path)?;
+        let markdown = format!(
+            "{}\n<div class=\"post-list\">\n{}</div>",
+            markdown,
+            comrak::markdown_to_html(
+                &posts
+                    .iter()
+                    .map(|p| format!(
+                        "* <div class=\"post-date\">{}</div> <a href=\"{}\">{}</a>",
+                        p.timestamp.format("%b %Y"),
+                        p.path,
+                        p.title
+                    ))
+                    .collect::<Vec<String>>()
+                    .join("\n"),
+                &self.options
+            )
+        );
+
+        let result = self.parse_markdown::<PageFrontmatter>(markdown)?;
+
+        let mut buf = Vec::new();
+        comrak::format_html(result.body, &self.options, &mut buf)?;
+        let html = String::from_utf8(buf)
+            .context("Failed to convert markdown to HTML (UTF8 validation failed)")?;
+
+        let html = Page {
+            title: result.frontmatter.title.clone(),
+            css: result.frontmatter.css.unwrap_or("".to_string()),
+            content: html,
+        }
+        .render()
+        .context("Failed to render template")?;
+
+        // Write the HTML to the output file
+        fs::write(output_path, &html)?;
+
+        tracing::info!("Markdown conversion completed successfully");
+        Ok(())
+    }
+
+    fn build_pages(&'a self) -> Result<()> {
+        // Ensure the output directory exists
+        let output_dir = Path::new("_site");
+        if !output_dir.exists() {
+            fs::create_dir_all(output_dir)?;
+        }
+
+        // Check if the posts directory exists
+        let pages_dir = Path::new("pages");
+        if !pages_dir.exists() {
+            anyhow::bail!("Pages directory not found");
+        }
+
+        // Use walkdir to iterate through all files in the posts directory
+        for entry in WalkDir::new(pages_dir)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file() && e.path() != Path::new("index.md"))
+        {
+            let path = entry.path();
+
+            // Only process .md files
+            if path.extension() != Some(OsStr::new("md")) {
+                continue;
+            }
+            // Get the relative path from the posts directory
+            let relative_path = path.strip_prefix(pages_dir)?;
+            let output_path = output_dir.join(relative_path.with_extension("html"));
+            let output_path2 = output_dir
+                .join(relative_path.with_extension(""))
+                .join("index.html");
+
+            // Ensure the parent directory exists
+            if let Some(parent) = output_path.parent() {
+                fs::create_dir_all(parent)?;
+            }
+            if let Some(parent) = output_path2.parent() {
+                fs::create_dir_all(parent)?;
+            }
+
+            tracing::info!("Converting {} to {}", path.display(), output_path.display());
+            tracing::info!(
+                "Converting {} to {}",
+                path.display(),
+                output_path2.display()
+            );
+
+            let mut markdown = fs::read_to_string(path)?;
+
+            let parsed = self.parse_markdown::<PageFrontmatter>(markdown)?;
+            let mut buf = Vec::new();
+            comrak::format_html(&parsed.body, &self.options, &mut buf)?;
+            let html = String::from_utf8(buf)
+                .context("Failed to convert markdown to HTML (UTF8 validation failed)")?;
+
+            let html = Page {
+                title: parsed.frontmatter.title.clone(),
+                css: parsed.frontmatter.css.unwrap_or("".to_string()),
+                content: html,
+            }
+            .render()
+            .context("Failed to render template")?;
+
+            // Write the HTML to the output file
+            fs::write(output_path, &html)?;
+            fs::write(output_path2, &html)?;
+        }
+
+        tracing::info!("Markdown conversion completed successfully");
+        Ok(())
+    }
+}
 struct PostMetadata {
     timestamp: NaiveDate,
     title: String,
@@ -365,8 +656,8 @@ fn build_pages(posts: Vec<PostMetadata>) -> Result<()> {
 pub fn build_all() -> Result<()> {
     build_assets()?;
     build_scss()?;
-    let mut posts = build_posts()?;
-    posts.sort_by_key(|p| std::cmp::Reverse(p.timestamp));
-    build_pages(posts)?;
+    let posts = build_posts()?;
+    build_index_html(posts)?;
+    build_pages()?;
     Ok(())
 }
