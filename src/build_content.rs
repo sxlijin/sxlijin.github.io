06ee1b81753c235e3b155787dd93fb82ea38f500
Sam Lijin <dev@sxlijin.com>
2025-04-26 21:17:53 -0700

    build content

--- src/build_content.rs
+++ src/build_content.rs
@@ -115,6 +115,162 @@ struct ParsedMarkdown<TFrontmatter: Default + DeserializeOwned> {
     html: String,
 }
 
+trait Collection {
+    fn input_paths() -> impl Iterator<Item = PathBuf>;
+
+    fn output_paths(input_path: &Path) -> Vec<PathBuf>;
+
+    fn render(
+        &mut self,
+        input_path: &Path,
+        parsed: ParsedMarkdown<PageFrontmatter>,
+        options: &comrak::Options,
+    ) -> Result<PartialPage>;
+}
+
+struct PostCollection {
+    posts: Vec<PostMetadata>,
+}
+
+impl Collection for PostCollection {
+    fn input_paths() -> impl Iterator<Item = PathBuf> {
+        WalkDir::new("posts")
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file())
+            .map(|e| e.path().to_path_buf())
+    }
+
+    fn output_paths(input_path: &Path) -> Vec<PathBuf> {
+        let input_path = input_path
+            .strip_prefix("posts")
+            .expect("Failed to strip 'posts' prefix from path in 'posts/'");
+        vec![
+            input_path.with_extension("html"),
+            input_path.with_extension("").join("index.html"),
+        ]
+    }
+
+    fn render(
+        &mut self,
+        input_path: &Path,
+        parsed: ParsedMarkdown<PageFrontmatter>,
+        _: &comrak::Options,
+    ) -> Result<PartialPage> {
+        let post_metadata = PostMetadata {
+            timestamp: NaiveDate::parse_from_str(
+                &input_path.file_name().unwrap().to_str().unwrap()[..10],
+                "%Y-%m-%d",
+            )?,
+            title: parsed.first_h1.clone(),
+            path: input_path
+                .file_stem()
+                .expect("Failed to get file stem")
+                .to_str()
+                .unwrap()
+                .to_string(),
+        };
+        let mut html = parsed.html.lines().collect::<Vec<&str>>();
+        let formatted_date = &format!("<p>{}</p>", post_metadata.timestamp.format("%Y %b %d"));
+        html.insert(1, formatted_date);
+        html.insert(2, "<hr/>");
+        let html = PartialPage {
+            title: post_metadata.title.clone(),
+            css: "".to_string(),
+            content: html.join("\n"),
+        };
+
+        self.posts.push(post_metadata);
+
+        Ok(html)
+    }
+}
+
+struct IndexHtmlCollection {
+    posts: Vec<PostMetadata>,
+}
+
+impl Collection for IndexHtmlCollection {
+    fn input_paths() -> impl Iterator<Item = PathBuf> {
+        std::iter::once(PathBuf::from("pages/index.md"))
+    }
+
+    fn output_paths(_: &Path) -> Vec<PathBuf> {
+        vec![PathBuf::from("index.html")]
+    }
+
+    fn render(
+        &mut self,
+        _: &Path,
+        parsed: ParsedMarkdown<PageFrontmatter>,
+        options: &comrak::Options,
+    ) -> Result<PartialPage> {
+        let post_list = format!(
+            "<div class=\"post-list\">\n{}</div>",
+            comrak::markdown_to_html(
+                &self
+                    .posts
+                    .iter()
+                    .map(|p| format!(
+                        "* <div class=\"post-date\">{}</div> <a href=\"{}\">{}</a>",
+                        p.timestamp.format("%b %Y"),
+                        p.path,
+                        p.title
+                    ))
+                    .collect::<Vec<String>>()
+                    .join("\n"),
+                options
+            )
+        );
+        let mut html = parsed.html.lines().collect::<Vec<&str>>();
+
+        // Find the index of the line containing "the first blog" and insert after it
+        if let Some(index) = html.iter().position(|line| line.contains("blog post list")) {
+            html.insert(index + 1, &post_list);
+        }
+
+        Ok(PartialPage {
+            title: parsed.frontmatter.title.unwrap_or(parsed.first_h1),
+            css: parsed.frontmatter.css.unwrap_or("".to_string()),
+            content: html.join("\n"),
+        })
+    }
+}
+
+struct PageCollection;
+impl Collection for PageCollection {
+    fn input_paths() -> impl Iterator<Item = PathBuf> {
+        WalkDir::new("pages")
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file())
+            .map(|e| e.path().to_path_buf())
+    }
+
+    fn output_paths(input_path: &Path) -> Vec<PathBuf> {
+        let input_path = input_path
+            .strip_prefix("pages")
+            .expect("Failed to strip 'pages' prefix from path in 'pages/'");
+        vec![
+            input_path.with_extension("html"),
+            input_path.with_extension("").join("index.html"),
+        ]
+    }
+
+    fn render(
+        &mut self,
+        _: &Path,
+        parsed: ParsedMarkdown<PageFrontmatter>,
+        _: &comrak::Options,
+    ) -> Result<PartialPage> {
+        Ok(PartialPage {
+            title: parsed.frontmatter.title.unwrap_or(parsed.first_h1),
+            css: parsed.frontmatter.css.unwrap_or("".to_string()),
+            content: parsed.html,
+        })
+    }
+}
+
 impl<'a, 'b> MarkdownRenderEngine<'a, 'b> {
     fn new() -> Self {
         Self {
@@ -171,6 +327,7 @@ impl<'a, 'b> MarkdownRenderEngine<'a, 'b> {
         })
     }
 
+    #[allow(dead_code)]
     fn build_posts(&'a self) -> Result<Vec<PostMetadata>> {
         let mut posts = Vec::new();
 
@@ -256,6 +413,7 @@ impl<'a, 'b> MarkdownRenderEngine<'a, 'b> {
         Ok(posts)
     }
 
+    #[allow(dead_code)]
     fn build_index_html(&'a self, posts: Vec<PostMetadata>) -> Result<()> {
         // Ensure the output directory exists
         let output_dir = Path::new("_site");
@@ -316,6 +474,7 @@ impl<'a, 'b> MarkdownRenderEngine<'a, 'b> {
         Ok(())
     }
 
+    #[allow(dead_code)]
     fn build_pages(&'a self) -> Result<()> {
         // Ensure the output directory exists
         let output_dir = Path::new("_site");
@@ -384,11 +543,62 @@ impl<'a, 'b> MarkdownRenderEngine<'a, 'b> {
         tracing::info!("Markdown conversion completed successfully");
         Ok(())
     }
+
+    fn build_collection<TCollection: Collection>(
+        &'a self,
+        collection: &mut TCollection,
+    ) -> Result<()> {
+        // Ensure the output directory exists
+        let output_dir = Path::new("_site");
+        if !output_dir.exists() {
+            fs::create_dir_all(output_dir)?;
+        }
+
+        // Use walkdir to iterate through all files in the posts directory
+        for input_path in TCollection::input_paths() {
+            // Get the relative path from the posts directory
+            let output_paths = TCollection::output_paths(&input_path);
+            tracing::info!("Converting {} to {:?}", input_path.display(), output_paths);
+
+            // Read the markdown file
+            let markdown = fs::read_to_string(&input_path)?;
+
+            let parsed = self.parse_markdown::<PageFrontmatter>(markdown)?;
+
+            let html = collection.render(&input_path, parsed, &self.options)?;
+
+            let html = Page {
+                title: html.title,
+                css: html.css,
+                content: html.content,
+                build_timestamp: EmbeddedBuildTimestamp(self.build_timestamp),
+            }
+            .render()
+            .context("Failed to render template")?;
+
+            // Write the HTML to the output file
+            for output_path in output_paths {
+                if let Some(parent) = output_path.parent() {
+                    fs::create_dir_all(parent)?;
+                }
+                fs::write(output_path, &html)?;
+            }
+        }
+
+        tracing::debug!("Markdown conversion completed successfully");
+        Ok(())
+    }
 }
 struct PostMetadata {
     timestamp: NaiveDate,
     title: String,
     path: String,
+}
+
+struct PartialPage {
+    title: String,
+    css: String,
+    content: String,
 }
 
 #[derive(Debug, askama::Template)]
@@ -418,9 +628,16 @@ pub fn build_website() -> Result<BuildSummary> {
     build_assets()?;
     build_scss()?;
     let engine = MarkdownRenderEngine::new();
-    let posts = engine.build_posts()?;
-    engine.build_index_html(posts)?;
-    engine.build_pages()?;
+    // let posts = engine.build_posts()?;
+    // engine.build_index_html(posts)?;
+    // engine.build_pages()?;
+
+    let mut posts = PostCollection { posts: vec![] };
+    engine.build_collection(&mut posts)?;
+    let mut index = IndexHtmlCollection { posts: posts.posts };
+    engine.build_collection(&mut index)?;
+    let mut pages = PageCollection {};
+    engine.build_collection(&mut pages)?;
 
     Ok(BuildSummary {
         build_timestamp: engine.build_timestamp,
