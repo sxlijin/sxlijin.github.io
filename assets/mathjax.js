5d7914753150cfaf03e786b7c76f27ca0a3dfc98
Sam Lijin <dev@sxlijin.com>
2025-04-26 23:55:23 -0700

    nbsp shenanigans

--- assets/mathjax.js
+++ assets/mathjax.js
@@ -11,6 +11,24 @@ window.MathJax = {
 					const mathText = span.textContent;
 					const isDisplay = span.getAttribute("data-math-style") === "display";
 					span.innerHTML = isDisplay ? `$$${mathText}$$` : `$${mathText}$`;
+
+					// If the math span is followed by punctuation, insert an NBSP to
+					// discourage line wrapping engines from wrapping between a math
+					// expression and punctuation, e.g. `$a+b$,` should not have a line
+					// break between the inline math and comma.
+					const nextNode = span.nextSibling;
+					if (nextNode && nextNode.nodeType === Node.TEXT_NODE) {
+						const nextText = nextNode.textContent;
+						if (nextText.match(/^[.,!?;:]/) !== null) {
+							// \u202f is NNBSP, narrow non-breaking space
+							// \u00a0, the NBSP, is a full-width space in theory
+							// \u2060 the zero-width nobreak should be used
+							// here. but chrome doesn't appear to handle it
+							// correctly because it will still insert a line
+							// break so :shrug:
+							nextNode.textContent = `\u202f${nextText}`;
+						}
+					}
 				}
 				// Typeset the new content
 				MathJax.typeset();
